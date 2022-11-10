3ae93f791cfd382c1d51da93ebf3a036b52e8097
clara <clara.chalayer@edu.ece.fr>
2022-11-10 10:08:33 +0100

    WIP TP5

---
+++ exo1.c
@@ -0,0 +1,19 @@
+//
+// Created by clara on 10/11/2022.
+//
+#include <stdio.h>
+#include "exo1.h"
+def
+int main(){
+    int a, int b, int c, int d;
+    printf("numerateur1:");
+    scanf("%d",&a);
+    printf("numerateur2:");
+    scanf("%d",&b);
+    printf("denominateur1:");
+    scanf("%d",&c);
+    printf("denominateur2:");
+    scanf("%d",&d);
+    return 0;
+}
+
---
+++ exo1.h
@@ -0,0 +1,9 @@
+//
+// Created by clara on 10/11/2022.
+//
+
+#ifndef TP5_EXO1_H
+#define TP5_EXO1_H
+int multiplication(int a, int b, int c, int d);
+int addition(int a, int b, int c, int d);
+#endif //TP5_EXO1_H
